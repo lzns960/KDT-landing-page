d12c3f4610da8e843377bdb03c98f833bdab088b
lzns960 <lzns960@gmail.com>
2022-07-26 20:36:55 +0900

    update weldcome

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const snbMenu = document.querySelector("#snb .sub_menu");
 const snbBtn = document.querySelector("#snb .btn_menu"); 
+const harryP = document.querySelector(".harryP");
 
 snbBtn.addEventListener("click", function(){
     console.log("!");
@@ -9,3 +10,9 @@ snbBtn.addEventListener("click", function(){
         snbMenu.classList.add("hide");
     }
 });
+
+harryP.onclick = function(){
+    this.textContent ="Welcome to Hogwarts!";
+    this.style.color="rgb(218,218,218)" ;
+    this.style.textShadow="2px 2px 5px silver";
+}
